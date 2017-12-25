89423c3379e9cb22aeba7382a8c8c6411da7ab6a
DenisAltruist <34612072+DenisAltruist@users.noreply.github.com>
2017-12-25 22:19:46 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,60 +1,50 @@
 #include <iostream>
 
-using namespace std;
-
 class ListElement {
  friend class List;
  private:
      int key;
      ListElement *ptr;
- public:
-     ListElement() {
-        ptr = nullptr;
-     }
 
-     ListElement(int value) {
-         key = value;
-         ptr = nullptr;
-     }
+ public:
+     ListElement() : ptr(nullptr) {};
+
+     ListElement(int value): key(value), ptr(nullptr) {};
 
      void SetKey(int value) {
         key = value;
      }
 
-     ListElement* getNext() const {
+     ListElement* GetNext() const {
         return ptr;
      }
 
-     int getKey() const {
+     int GetKey() const {
         return key;
      }
 };
 
 class List {
- friend void MergeSort(List& A, int length);
+ friend class MergeSorter;
  private:
     ListElement *first, *last;
     bool isEmpty;
 
  public:
-     List() {
-        isEmpty = true;
-        first = nullptr;
-        last = nullptr;
-     }
+     List(): first(nullptr), last(nullptr), isEmpty(true) {};
 
-    ListElement* getFirst() const {
+    ListElement* GetFirst() const {
         return first;
     }
 
-    ListElement* getLast() const {
+    ListElement* GetLast() const {
         return last;
     }
 
     void SetFree() {
         while (first != nullptr) {
             ListElement* temp = first;
-            first = first -> ptr;
+            first = first->ptr;
             delete temp;
         }
         last = nullptr;
@@ -65,11 +55,11 @@ class List {
         SetFree();
         ListElement* currPtr = newFirst;
         while (1) {
-            Push(currPtr -> getKey());
+            Push(currPtr->GetKey());
             if (currPtr == newLast) {
                 break;
             }
-            currPtr = currPtr -> getNext();
+            currPtr = currPtr->GetNext();
         }
     }
 
@@ -79,85 +69,86 @@ class List {
             last = first;
             isEmpty = false;
         } else {
-            last -> ptr = new ListElement(value);
-            last = last -> ptr;
+            last->ptr = new ListElement(value);
+            last = last->ptr;
         }
     }
 
-    void operator =(const List& rhs) {
+    void operator= (const List& rhs) {
         SetFree();
-        ListElement* p = rhs.getFirst();
+        ListElement* p = rhs.GetFirst();
         while (p != nullptr) {
-            Push(p -> key);
-            p = p -> ptr;
+            Push(p->key);
+            p = p->ptr;
         }
     }
     ~List() {
         SetFree();
     }
-
-
 };
 
-ostream& operator <<(ostream& out, const List& A) {
-    ListElement* ptr = A.getFirst();
+std::ostream& operator<< (std::ostream& out, const List& A) {
+    ListElement* ptr = A.GetFirst();
     while (ptr != nullptr) {
-        out << ptr -> getKey() << " ";
-        ptr = ptr -> getNext();
+        out << ptr->GetKey() << " ";
+        ptr = ptr->GetNext();
     }
     return out;
 }
 
+class MergeSorter {
+ private:
+    List T;
 
-List T;
-
-void MergeSort(ListElement* lp, ListElement* rp, int length) {
-    if (length == 1) {
-        return;
-    }
-    int lengthOfTheLeftPart = (length >> 1);
-    ListElement* mp = lp;
-    for (int i = 0; i < lengthOfTheLeftPart - 1; ++i) {
-        mp = mp -> getNext();
-    }
-    MergeSort(lp, mp, lengthOfTheLeftPart);
-    mp = mp -> getNext();
-    MergeSort(mp, rp, length - lengthOfTheLeftPart);
-    T.SetFree();
-
-    ListElement* ip = lp;
-    ListElement* jp = mp;
-    int cntOfStepsIp = lengthOfTheLeftPart;
-    int cntOfStepsJp = length - cntOfStepsIp;
-    while (cntOfStepsIp != 0 || cntOfStepsJp != 0) {
-        if ((ip != mp) && ((cntOfStepsJp == 0) || (ip -> getKey() < jp -> getKey()))) {
-            T.Push(ip -> getKey());
-            ip = ip -> getNext();
-            --cntOfStepsIp;
-        } else {
-            T.Push(jp -> getKey());
-            jp = jp -> getNext();
-            --cntOfStepsJp;
+ public:
+    void Sort(ListElement* lp, ListElement* rp, int length) {
+        if (length == 1) {
+            return;
+        }
+        int lengthOfTheLeftPart = (length >> 1);
+        ListElement* mp = lp;
+        for (int i = 0; i < lengthOfTheLeftPart - 1; ++i) {
+            mp = mp->GetNext();
+        }
+        Sort(lp, mp, lengthOfTheLeftPart);
+        mp = mp->GetNext();
+        Sort(mp, rp, length - lengthOfTheLeftPart);
+        T.SetFree();
+        ListElement* ip = lp;
+        ListElement* jp = mp;
+        int cntOfStepsIp = lengthOfTheLeftPart;
+        int cntOfStepsJp = length - cntOfStepsIp;
+        while (cntOfStepsIp != 0 || cntOfStepsJp != 0) {
+            if ((ip != mp) && ((cntOfStepsJp == 0) || (ip->GetKey() < jp->GetKey()))) {
+                T.Push(ip->GetKey());
+                ip = ip->GetNext();
+                --cntOfStepsIp;
+            } else {
+                T.Push(jp->GetKey());
+                jp = jp->GetNext();
+                --cntOfStepsJp;
+            }
+        }
+        ListElement* ptr = T.GetFirst();
+        while (ptr != nullptr) {
+            lp->SetKey(ptr->GetKey());
+            lp = lp->GetNext();
+            ptr = ptr->GetNext();
         }
     }
-    ListElement* ptr = T.getFirst();
-    while (ptr != nullptr) {
-        lp -> SetKey(ptr -> getKey());
-        lp = lp -> getNext();
-        ptr = ptr -> getNext();
-    }
-}
+};
 
 int main() {
     int n;
-    cin >> n;
+    std::cin >> n;
     List A;
     for (int i = 0; i < n; ++i) {
         int x;
-        cin >> x;
+        std::cin >> x;
         A.Push(x);
     }
-    MergeSort(A.getFirst(), A.getLast(), n);
-    cout << A << "\n";
+    MergeSorter MS;
+    MS.Sort(A.GetFirst(), A.GetLast(), n);
+    std::cout << A << "\n";
     return 0;
 }
